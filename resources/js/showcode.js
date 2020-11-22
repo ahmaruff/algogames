d815647c12bf990fbdc1f6642a4eb8f0ccf04cff
ahmaruff <ahmadmaruf2701@gmail.com>
2020-11-22 22:49:16 +0700

    add blockly basic functionality

---
+++ resources/js/showcode.js
@@ -0,0 +1,10 @@
+function showCode() {
+    // Generate JavaScript code and display it.
+    Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    alert(code);
+}
+
+function hh() {
+    alert("hellowrold")
+}
